7daf6d5e762fe691ee70ac6c8c56c1bd4defe429
Gaëtan <61387206+GaetanOff@users.noreply.github.com>
2025-04-01 17:24:26 +0200

    style: better preferences menu

--- LookAway/Views/PreferencesView.swift
+++ LookAway/Views/PreferencesView.swift
@@ -9,8 +9,10 @@ struct PreferencesView: View {
                 Spacer()
                 Text("Break Settings")
                     .font(.headline)
+                Spacer()
             }
             .padding(.bottom, 5)
+            .padding(.top, 2)
             
             HStack {
                 Text("Break Every:")
@@ -36,13 +38,14 @@ struct PreferencesView: View {
                 Spacer()
                 Text("Overlay Text")
                     .font(.headline)
+                Spacer()
             }
             .padding(.top, 10)
             .padding(.bottom, 5)
             
             HStack {
                 Text("Title:")
-                    .frame(width: 120, alignment: .trailing)
+                    .frame(width: 120, alignment: .leading)
                 TextField("Relax those eyes", text: $settings.overlayTitle)
                     .textFieldStyle(PlainTextFieldStyle())
                     .frame(height: 24)
@@ -53,7 +56,7 @@ struct PreferencesView: View {
             
             HStack {
                 Text("Description:")
-                    .frame(width: 120, alignment: .trailing)
+                    .frame(width: 120, alignment: .leading)
                 TextField("Choose a distant view to set your sight on until the timer ends", text: $settings.overlayDescription)
                     .textFieldStyle(PlainTextFieldStyle())
                     .frame(height: 24)
@@ -66,30 +69,41 @@ struct PreferencesView: View {
                 Spacer()
                 Text("Status Bar")
                     .font(.headline)
+                Spacer()
             }
             .padding(.top, 10)
             .padding(.bottom, 5)
             
             HStack {
                 Text("Status Bar Suffix:")
-                    .frame(width: 120, alignment: .trailing)
-                Text("min")
-                    .foregroundColor(.secondary)
-                TextField("| Eyes", text: $settings.statusSuffix)
-                    .textFieldStyle(PlainTextFieldStyle())
-                    .frame(width: 100, height: 24)
-                    .padding(4)
-                    .background(Color(NSColor.textBackgroundColor))
-                    .cornerRadius(4)
+                    .frame(width: 120, alignment: .leading)
+                Spacer()
+                HStack(spacing: 4) {
+                    TextField("| Eyes", text: $settings.statusSuffix)
+                        .textFieldStyle(PlainTextFieldStyle())
+                        .frame(width: 100, height: 24)
+                        .padding(4)
+                        .background(Color(NSColor.textBackgroundColor))
+                        .cornerRadius(4)
+                }
+                .frame(width: 155, alignment: .trailing)
             }
             
-            Section() {
+            HStack {
+                Spacer()
+                Text("Other options")
+                    .font(.headline)
+                Spacer()
+            }
+            .padding(.top, 10)
+            .padding(.bottom, 5)
+            
+            HStack {
                 Toggle("Reset Timer on Sleep/Lock", isOn: $settings.resetTimerOnSleep)
-                    .onChange(of: settings.resetTimerOnSleep) { newValue, _ in
-                    }
+                    .frame(width: 275, alignment: .leading)
+                Spacer()
             }
 
-            
             Spacer()
             
             HStack {
@@ -107,7 +121,7 @@ struct PreferencesView: View {
             .padding(.bottom, 10)
         }
         .padding()
-        .frame(width: 500, height: 480)
+        .frame(width: 500, height: 550)
         .background(Color(NSColor.windowBackgroundColor))
     }
 }
